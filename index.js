5492007c35a1cf541bab001e7f67b3b48cddd299
Uu2134 <115209535+Uu2134@users.noreply.github.com>
2024-09-27 11:25:56 +0500

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const conversationsRouter = require('./api/conversations');
+const conversationsRouter = require('./api/conversations/conversations');
 
 // Middleware to parse JSON requests
 app.use(express.json());
